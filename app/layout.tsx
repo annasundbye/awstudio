918f60bd9993b424962c21c60369f845d2247e57
Anna Sundbye <annasundbye@Anna-sin-MacBook-Air.local>
2026-04-27 19:41:25 +0200

    add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import SwipeNavigation from "@/components/SwipeNavigation";
 import AnalyticsProvider from "@/hooks/analytics";
 import CookieBanner from "@/components/CookieBanner";
 import { GoogleAnalyticsScripts } from "@/hooks/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,23 +26,19 @@ export default function RootLayout({
   return (
     <html lang="no">
       <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5QH7L8RH');</script>
+        <GoogleTagManager gtmId="GTM-5QH7L8RH" />
         <link rel="shortcut icon" href="/signatur2.png" type="image/x-icon" />
         <GoogleAnalyticsScripts />
       </head>
       <body className={inter.className}>
-      <noscript>
-    <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-5QH7L8RH"
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-    />
-  </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QH7L8RH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
